9fe5833c1db1759a2cb4199637c6bea316feed6e
Amir <amir.abdullaev@email.cz>
2022-03-06 00:44:24 +0100

    learn comment and variables

--- src/com/codewithamir/Main.java
+++ src/com/codewithamir/Main.java
@@ -3,7 +3,34 @@ package com.codewithamir;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // this is single-line commet
+        /* this is multi-line commet */
         System.out.println("Hello world");
+        // variables string, int, float
+        // string
+        String name = "Amir";
+        System.out.println(name);
+
+        // int
+        int myNum = 15;
+        System.out.println(myNum);
+
+        // float
+        float myFloatNum = 54.3f;
+        System.out.println(myFloatNum);
+
+        // display variables
+        String text = "Amir";
+        System.out.println("Hello " + text);
+
+        String firstName = "Amiriddin ";
+        String lastName = "Abdullaev";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 63;
+        int y = 89;
+        int z = 32;
+        System.out.println(x + y + z);
     }
 }
