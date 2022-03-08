19ed15cfc8a21447a3f1bc4fc24c4b99e49d886d
Amir <amir.abdullaev@email.cz>
2022-03-08 11:09:18 +0100

    java strings

---
+++ src/com/codewithamir/strings.java
@@ -0,0 +1,22 @@
+package com.codewithamir;
+
+import java.util.Locale;
+
+public class strings {
+    public static void main(String[] args) {
+        //Method
+            // length
+            String text = "qwertyuiopasdfg";
+            System.out.println("The length of the text sting is: " + text.length());
+            //Upper case
+            String txt = "Amiriddin";
+            System.out.println(txt.toUpperCase(Locale.ROOT));
+            System.out.println(txt.toLowerCase(Locale.ROOT));
+
+
+            //string concatenation
+            String lastName = "Abdullaev";
+            String firstName = "Amiriddin ";
+            System.out.println(firstName.concat(lastName));
+    }
+}
