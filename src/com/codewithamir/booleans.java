86b185b55394686ff5efa95feb8eafa532460bcf
Amir <amir.abdullaev@email.cz>
2022-03-09 13:59:59 +0100

    create class booleans and learning booleans

---
+++ src/com/codewithamir/booleans.java
@@ -0,0 +1,14 @@
+package com.codewithamir;
+
+public class booleans {
+    public static void main(String[] args) {
+        int x = 45;
+        int y = 38;
+        System.out.println(x > y);    //retruns true, because 45 higher than 38
+        // the same is with if you write there numbers
+
+        int z = 453;
+        System.out.println(z == 234);  // returns false because the value z is higher than 234
+        // the same is with if you write there 453 == 234
+    }
+}
