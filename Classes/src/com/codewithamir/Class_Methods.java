1149de23587436e7cc7b6189444aad9eb0136999
Amir <amir.abdullaev@email.cz>
2022-04-17 22:53:51 +0200

    create class Class_Methods and start learning methods within class

---
+++ Classes/src/com/codewithamir/Class_Methods.java
@@ -0,0 +1,12 @@
+package com.codewithamir;
+
+public class Class_Methods {
+    //This is method
+    static void myMethod() {
+        System.out.println("Hello Amir");
+    }
+
+    public static void main(String[] args) {
+        myMethod();  //will print Hello Amir
+    }
+}
